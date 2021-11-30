e5e925c408c3dd2663855f3b46bd44bc917d751e
Opilane TTHK <opilane@tthk.ee>
2021-11-30 13:33:46 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "admin";
+            string password = "pass1234";
+            string UserName, userpassword;
+            int tries = 0;
+
+            while(tries !=3)
+
+            Console.WriteLine("enter your username: ");
+
+
+            UserName = Console.ReadLine();
+            Console.WriteLine("enter your password: ");
+            userpassword = Console.ReadLine();
+
+            if (login != UserName || password != userpassword)
+                Console.WriteLine("Oops something went wrong");
+            tries++;
+
+        }
+    }
+}
